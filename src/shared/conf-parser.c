7729c3555b105af1cdbb6fd8da005c8e2f5543ef
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2014-03-04 17:11:39 +0100

    Do not print invalid UTF-8 in error messages
    Inexplicably, 550a40ec ('core: do not print invalid utf-8 in error
    messages') only fixed two paths. Convert all of them now.
    
    (cherry picked from commit b5d742138f71e87312541a89aac5657015f50f48)
    
    [patch was slightly modified for context adjustment]

--- src/shared/conf-parser.c
+++ src/shared/conf-parser.c
@@ -618,8 +618,7 @@ int config_parse_string(const char *unit,
                 return log_oom();
 
         if (!utf8_is_valid(n)) {
-                log_syntax(unit, LOG_ERR, filename, line, EINVAL,
-                           "String is not UTF-8 clean, ignoring assignment: %s", rvalue);
+		log_invalid_utf8(unit, LOG_ERR, filename, line, EINVAL, rvalue);
                 free(n);
                 return 0;
         }
@@ -656,8 +655,7 @@ int config_parse_path(const char *unit,
         assert(data);
 
         if (!utf8_is_valid(rvalue)) {
-                log_syntax(unit, LOG_ERR, filename, line, EINVAL,
-                           "Path is not UTF-8 clean, ignoring assignment: %s", rvalue);
+		log_invalid_utf8(unit, LOG_ERR, filename, line, EINVAL, rvalue);
                 return 0;
         }
 
@@ -725,8 +723,7 @@ int config_parse_strv(const char *unit,
                         return log_oom();
 
                 if (!utf8_is_valid(n)) {
-                        log_syntax(unit, LOG_ERR, filename, line, EINVAL,
-                                   "String is not UTF-8 clean, ignoring: %s", rvalue);
+                        log_invalid_utf8(unit, LOG_ERR, filename, line, EINVAL, rvalue);
                         continue;
                 }
 
