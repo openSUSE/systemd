f938b45bbbf6f76dec6975d94c95d29450058ed9
Chris Leech <cleech@redhat.com>
2014-11-23 20:33:38 -0800

    mount: check options as well as fstype for network mounts
    When creating a new mount unit after an event on /proc/self/mountinfo,
    check the mount options as well as the fstype to determine if this is a
    remote mount that requires network access.
    
    (cherry picked from commit affc3d834347076e8616948978e70ed1fca84db4)
    
    [fbui: adjust context]

--- src/core/mount.c
+++ src/core/mount.c
@@ -76,16 +76,20 @@ static char* mount_test_option(const char *haystack, const char *needle) {
         return hasmntopt(&me, needle);
 }
 
+static bool mount_needs_network(const char *options, const char *fstype) {
+        if (mount_test_option(options, "_netdev"))
+                 return true;
+
+        if (fstype && fstype_is_network(fstype))
+                 return true;
+
+        return false;
+}
+
 static bool mount_is_network(MountParameters *p) {
         assert(p);
 
-        if (mount_test_option(p->options, "_netdev"))
-                return true;
-
-        if (p->fstype && fstype_is_network(p->fstype))
-                return true;
-
-        return false;
+        return mount_needs_network(p->options, p->fstype);
 }
 
 static bool mount_is_loop(const MountParameters *p) {
@@ -1497,8 +1501,7 @@ static int mount_setup_unit(
                 if (m->running_as == SYSTEMD_SYSTEM) {
                         const char* target;
 
-                        target = fstype_is_network(fstype) ? SPECIAL_REMOTE_FS_TARGET : SPECIAL_LOCAL_FS_TARGET;
-
+                        target = mount_needs_network(options, fstype) ?  SPECIAL_REMOTE_FS_TARGET : SPECIAL_LOCAL_FS_TARGET;
                         r = unit_add_dependency_by_name(u, UNIT_BEFORE, target, NULL, true);
                         if (r < 0)
                                 goto fail;
