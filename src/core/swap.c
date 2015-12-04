openSUSE/systemd

commit 64347fc2b983f33e7efb0fd2bb44e133fb9f30f4

swap: handle nofail/noauto in core

src/core/swap.c
@@ -197,6 +197,7 @@ static int swap_add_device_links(Swap *s) {
 }
 
 static int swap_add_default_dependencies(Swap *s) {
+        bool nofail = false, noauto = false;
         int r;
 
         assert(s);
@@ -210,6 +211,20 @@ static int swap_add_default_dependencies(Swap *s) {
         r = unit_add_two_dependencies_by_name(UNIT(s), UNIT_BEFORE, UNIT_CONFLICTS, SPECIAL_UMOUNT_TARGET, NULL, true);
         if (r < 0)
                 return r;
+
+        if (s->from_fragment) {
+                SwapParameters *p = &s->parameters_fragment;
+
+                nofail = p->nofail;
+                noauto = p->noauto;
+        }
+
+        if (!noauto) {
+                r = unit_add_two_dependencies_by_name(UNIT(s), UNIT_BEFORE, (nofail ? UNIT_WANTED_BY : UNIT_REQUIRED_BY),
+                                                      SPECIAL_SWAP_TARGET, NULL, true);
+                if (r < 0)
+                        return r;
+        }
 
         return 0;
 }

src/fstab-generator/fstab-generator.c
@@ -38,22 +38,6 @@
 static const char *arg_dest = "/tmp";
 static bool arg_enabled = true;
 
-static int device_name(const char *path, char **unit) {
-        char *p;
-
-        assert(path);
-
-        if (!is_device_path(path))
-                return 0;
-
-        p = unit_name_from_path(path, ".device");
-        if (!p)
-                return log_oom();
-
-        *unit = p;
-        return 1;
-}
-
 static int mount_find_pri(struct mntent *me, int *ret) {
         char *end, *pri;
         unsigned long r;
@@ -82,7 +66,6 @@ static int mount_find_pri(struct mntent *me, int *ret) {
 static int add_swap(const char *what, struct mntent *me) {
         _cleanup_free_ char *name = NULL, *unit = NULL, *lnk = NULL, *device = NULL;
         _cleanup_fclose_ FILE *f = NULL;
-        bool noauto, nofail;
         int r, pri = -1;
 
         assert(what);
@@ -93,9 +76,6 @@ static int add_swap(const char *what, struct mntent *me) {
                 log_error("Failed to parse priority");
                 return pri;
         }
-
-        noauto = !!hasmntopt(me, "noauto");
-        nofail = !!hasmntopt(me, "nofail");
 
         name = unit_name_from_path(what, ".swap");
         if (!name)
@@ -114,14 +94,10 @@ static int add_swap(const char *what, struct mntent *me) {
                 return -errno;
         }
 
-        fputs("# Automatically generated by systemd-fstab-generator\n\n"
-              "[Unit]\n"
-              "SourcePath=/etc/fstab\n", f);
-
-        if (!noauto && !nofail)
-                fputs("Before=" SPECIAL_SWAP_TARGET "\n", f);
-
         fprintf(f,
+                "# Automatically generated by systemd-fstab-generator\n\n"
+                "[Unit]\n"
+                "SourcePath=/etc/fstab\n"
                 "\n"
                 "[Swap]\n"
                 "What=%s\n",
@@ -136,35 +112,6 @@ static int add_swap(const char *what, struct mntent *me) {
         if (ferror(f)) {
                 log_error("Failed to write unit file %s: %m", unit);
                 return -errno;
-        }
-
-        if (!noauto) {
-                lnk = strjoin(arg_dest, "/" SPECIAL_SWAP_TARGET ".wants/", name, NULL);
-                if (!lnk)
-                        return log_oom();
-
-                mkdir_parents_label(lnk, 0755);
-                if (symlink(unit, lnk) < 0) {
-                        log_error("Failed to create symlink %s: %m", lnk);
-                        return -errno;
-                }
-
-                r = device_name(what, &device);
-                if (r < 0)
-                        return r;
-
-                if (r > 0) {
-                        free(lnk);
-                        lnk = strjoin(arg_dest, "/", device, ".wants/", name, NULL);
-                        if (!lnk)
-                                return log_oom();
-
-                        mkdir_parents_label(lnk, 0755);
-                        if (symlink(unit, lnk) < 0) {
-                                log_error("Failed to create symlink %s: %m", lnk);
-                                return -errno;
-                        }
-                }
         }
 
         return 0;

commit 42e6da2cd5cba9adde3d4a343e3aa8b09a9ee592

fstab-generator: restore parsing and handling of 'nofail'/'noauto' options for swap devices
64347fc2b983f3 broke the handling of 'nofail', 'noauto' options for
swap devices. The handling of those options was moved in core but we
still had to parse and write the option values in the generated unit
file.

Actually 'nofail' option wasn't handled correctly even before the
broken commit...

This patch moved back the handling of those options from core to the
fstab generator (as it's now done by upstream) and really takes them
into account.

src/core/swap.c
@@ -210,7 +210,6 @@ static int swap_add_device_links(Swap *s) {
 }
 
 static int swap_add_default_dependencies(Swap *s) {
-        bool nofail, noauto;
         int r;
 
         assert(s);
@@ -221,29 +220,7 @@ static int swap_add_default_dependencies(Swap *s) {
         if (detect_container(NULL) > 0)
                 return 0;
 
-        r = unit_add_two_dependencies_by_name(UNIT(s), UNIT_BEFORE, UNIT_CONFLICTS, SPECIAL_UMOUNT_TARGET, NULL, true);
-        if (r < 0)
-                return r;
-
-        if (!s->from_fragment)
-                /* The swap unit can either be for an alternative device name, in which
-                 * case we don't need to add the dependency on swap.target because this unit
-                 * is following a different unit which will have this dependency added,
-                 * or it can be derived from /proc/swaps, in which case it was started
-                 * manually, and should not become a dependency of swap.target. */
-                return 0;
-
-        nofail = s->parameters_fragment.nofail;
-        noauto = s->parameters_fragment.noauto;
-
-        if (!noauto) {
-                if (nofail)
-                        r = unit_add_dependency_by_name_inverse(UNIT(s), UNIT_WANTS, SPECIAL_SWAP_TARGET, NULL, true);
-                else
-                        r = unit_add_two_dependencies_by_name_inverse(UNIT(s), UNIT_AFTER, UNIT_REQUIRES, SPECIAL_SWAP_TARGET, NULL, true);
-        }
-
-        return r < 0 ? r : 0;
+        return unit_add_two_dependencies_by_name(UNIT(s), UNIT_BEFORE, UNIT_CONFLICTS, SPECIAL_UMOUNT_TARGET, NULL, true);
 }
 
 static int swap_verify(Swap *s) {

src/fstab-generator/fstab-generator.c
@@ -109,7 +109,7 @@ static int mount_find_pri(struct mntent *me, int *ret) {
 static int add_swap(const char *what, struct mntent *me) {
         _cleanup_free_ char *name = NULL, *unit = NULL, *lnk = NULL;
         _cleanup_fclose_ FILE *f = NULL;
-        bool noauto;
+        bool noauto, nofail;
         int r, pri = -1;
 
         assert(what);
@@ -122,6 +122,7 @@ static int add_swap(const char *what, struct mntent *me) {
         }
 
         noauto = !!hasmntopt(me, "noauto");
+        nofail = !!hasmntopt(me, "nofail");
 
         name = unit_name_from_path(what, ".swap");
         if (!name)
@@ -160,7 +161,8 @@ static int add_swap(const char *what, struct mntent *me) {
         }
 
         if (!noauto) {
-                lnk = strjoin(arg_dest, "/" SPECIAL_SWAP_TARGET ".wants/", name, NULL);
+                lnk = strjoin(arg_dest, "/" SPECIAL_SWAP_TARGET,
+                              nofail ? ".wants/" : ".requires/", name, NULL);
                 if (!lnk)
                         return log_oom();
 