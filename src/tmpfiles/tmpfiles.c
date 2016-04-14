fef1086bad20222c1fba27bde42bdf3558a6c501
Thomas Blume <Thomas.Blume@suse.com>
2016-04-14 15:42:02 +0200

    tmpfiles: support exclude statements based on file ownership
    SUSE supported tmpfile cleanups based on file ownership before systemd.
    So this feature needs to be available in systemd.
    This was part of fate#314974
    
    [tblume: suse-only patch ported from SLES12-SP1 commit e769a63907ae4b]
    [tblume: part of fate#314974]

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -34,6 +34,8 @@
 #include <sys/xattr.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 #include "acl-util.h"
 #include "alloc-util.h"
@@ -384,6 +386,7 @@ static int dir_cleanup(
                 struct stat s;
                 usec_t age;
                 _cleanup_free_ char *sub_path = NULL;
+                Item *found;
 
                 if (dot_or_dot_dot(dent->d_name))
                         continue;
@@ -428,15 +431,41 @@ static int dir_cleanup(
                         goto finish;
                 }
 
-                /* Is there an item configured for this path? */
-                if (ordered_hashmap_get(items, sub_path)) {
-                        log_debug("Ignoring \"%s\": a separate entry exists.", sub_path);
-                        continue;
-                }
+                /* evaluate username arguments in ignore statements */
+                found = find_glob(globs, sub_path);
 
-                if (find_glob(globs, sub_path)) {
-                        log_debug("Ignoring \"%s\": a separate glob exists.", sub_path);
-                        continue;
+                if (i->type == CREATE_DIRECTORY && found && found->argument) {
+                        struct passwd *pw;
+                        char *userfound = NULL, *args = strdup(found->argument);
+                        bool match = false;
+
+                        while ((userfound = strsep(&args, ","))) {
+                                pw = getpwnam(userfound);
+
+                                if (pw) {
+                                        if (s.st_uid == pw->pw_uid) {
+                                                match = true;
+                                                break;
+                                        }
+                                }
+                        }
+
+                        if (match) {
+                                log_debug("Ignoring \"%s\" of user \"%s\".", sub_path, pw->pw_name);
+                                match=false;
+                                continue;
+                        }
+                } else {
+                        /* Is there an item configured for this path? */
+                        if (ordered_hashmap_get(items, sub_path)) {
+                                log_debug("Ignoring \"%s\": a separate entry exists.", sub_path);
+                                continue;
+                        }
+
+                        if (found) {
+                                log_debug("Ignoring \"%s\": a separate glob exists.", sub_path);
+                                continue;
+                        }
                 }
 
                 if (S_ISDIR(s.st_mode)) {
@@ -1839,8 +1868,6 @@ static int parse_line(const char *fname, unsigned line, const char *buffer) {
         case EMPTY_DIRECTORY:
         case TRUNCATE_DIRECTORY:
         case CREATE_FIFO:
-        case IGNORE_PATH:
-        case IGNORE_DIRECTORY_PATH:
         case REMOVE_PATH:
         case RECURSIVE_REMOVE_PATH:
         case ADJUST_MODE:
@@ -1851,6 +1878,8 @@ static int parse_line(const char *fname, unsigned line, const char *buffer) {
 
                 break;
 
+        case IGNORE_PATH:
+        case IGNORE_DIRECTORY_PATH:
         case CREATE_FILE:
         case TRUNCATE_FILE:
                 break;
