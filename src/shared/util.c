a468dedc83b7145c70073cfa8203e4c2f39e8a02
Franck Bui <fbui@suse.com>
2016-05-31 16:01:48 +0200

    Revert "0001-add-network-device-after-NFS-mount-units"
    This reverts commit 61210a1eb7aefa765c8a7d3d9dfb19eb7a288c64.
    
    After reading bug#861489, here are some facts:
    
      - bug was opened against 13.1 (v208)
      - this patch doesn't address/fix the bug
      - no fixes were provided for v208
      - it was simply assumed to be fixed somehow when upgrading v210
      - the hang during shutdown couldn't be reproduced with latest SP1
    
    See discussion:
    http://mailman.suse.de/mailman/private/systemd-maintainers/2016-May/019540.html
    for more details.

--- src/shared/util.c
+++ src/shared/util.c
@@ -1549,7 +1549,6 @@ bool fstype_is_network(const char *fstype) {
                 "ncp\0"
                 "nfs\0"
                 "nfs4\0"
-                "afs\0"
                 "gfs\0"
                 "gfs2\0";
 
