500dee3be5a373f4cab686413380a367f30e2450
Lennart Poettering <lennart@poettering.net>
2016-11-24 21:03:36 +0100

    nspawn: properly handle image/directory paths that are symlinks
    This resolves any paths specified on --directory=, --template=, and --image=
    before using them. This makes sure nspawn can be used correctly on symlinked
    images and directory trees.
    
    Fixes: #2001
    
    (cherry picked from commit 3f342ec4b028914fa558855609cd932364558c05)
    
    [fbui: adjust context: chase_symlinks() is missing in v228. Instead of
           backporting this fair chunck of new code, use
           canonicalize_file_name() instead as the 'root' parameter is not
           used in this case.]
    
    [fbui: fixes bsc#1012390]

--- src/nspawn/nspawn.c
+++ src/nspawn/nspawn.c
@@ -2375,6 +2375,24 @@ static int determine_names(void) {
         return 0;
 }
 
+static int chase_symlinks_and_update(char **p) {
+        char *chased;
+
+        assert(p);
+
+        if (!*p)
+                return 0;
+
+        chased = canonicalize_file_name(*p);
+        if (!chased)
+                return log_error_errno(errno, "Failed to resolve path %s: %m", *p);
+
+        free(*p);
+        *p = chased;
+
+        return 0;
+}
+
 static int determine_uid_shift(const char *directory) {
         int r;
 
@@ -3087,6 +3105,10 @@ int main(int argc, char *argv[]) {
                 if (arg_ephemeral) {
                         _cleanup_free_ char *np = NULL;
 
+                        r = chase_symlinks_and_update(&arg_directory);
+                        if (r < 0)
+                                goto finish;
+
                         /* If the specified path is a mount point we
                          * generate the new snapshot immediately
                          * inside it under a random name. However if
@@ -3137,6 +3159,10 @@ int main(int argc, char *argv[]) {
                         }
 
                         if (arg_template) {
+                                r = chase_symlinks_and_update(&arg_template);
+                                if (r < 0)
+                                        goto finish;
+
                                 r = btrfs_subvol_snapshot(arg_template, arg_directory, (arg_read_only ? BTRFS_SNAPSHOT_READ_ONLY : 0) | BTRFS_SNAPSHOT_FALLBACK_COPY | BTRFS_SNAPSHOT_RECURSIVE | BTRFS_SNAPSHOT_QUOTA);
                                 if (r == -EEXIST) {
                                         if (!arg_quiet)
@@ -3149,6 +3175,10 @@ int main(int argc, char *argv[]) {
                                                 log_info("Populated %s from template %s.", arg_directory, arg_template);
                                 }
                         }
+
+                        r = chase_symlinks_and_update(&arg_directory);
+                        if (r < 0)
+                                goto finish;
                 }
 
                 if (arg_boot) {
@@ -3173,6 +3203,10 @@ int main(int argc, char *argv[]) {
 
                 assert(arg_image);
                 assert(!arg_template);
+
+                r = chase_symlinks_and_update(&arg_image);
+                if (r < 0)
+                        goto finish;
 
                 r = image_path_lock(arg_image, (arg_read_only ? LOCK_SH : LOCK_EX) | LOCK_NB, &tree_global_lock, &tree_local_lock);
                 if (r == -EBUSY) {
