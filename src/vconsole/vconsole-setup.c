376d8c2a4727ade97b753c6af5cea19c4efb7b23
Michal Soltys <soltys@ziu.info>
2017-03-20 10:20:34 +0100

    vconsole: don't do GIO_SCRNMAP / GIO_UNISCRNMAP
    GIO_SCRNMAP / GIO_UNISCRNMAP are related to what setfont does with -m
    option - namely setting intermediate map from 8bit values into unicode
    values. This map is global, so single setfont invocation sets it for
    all applicable consoles.
    
    Furthermore calling GIO_SCRNMAP before GIO_UNISCRNMAP causes issues as
    the former corrupts values > 255 (UNI alone would be sufficient).
    
    The bug can be easily tested with the following conf:
    
    KEYMAP=pl
    FONT=LatArCyrHeb-16
    FONT_MAP=8859-2
    
    (cherry picked from commit c2f2c51c53e91232e6f2aa686d734728ad703f9f)
    
    [fbui: fixes bsc#1029691]
    [fbui: fixes bsc#904214]

--- src/vconsole/vconsole-setup.c
+++ src/vconsole/vconsole-setup.c
@@ -187,8 +187,6 @@ static int font_load(const char *vc, const char *font, const char *map, const ch
  */
 static void font_copy_to_all_vcs(int fd) {
         struct vt_stat vcs = {};
-        unsigned char map8[E_TABSZ];
-        unsigned short map16[E_TABSZ];
         struct unimapdesc unimapd;
         struct unipair unipairs[USHRT_MAX];
         int i, r;
@@ -220,14 +218,6 @@ static void font_copy_to_all_vcs(int fd) {
                 cfo.op = KD_FONT_OP_COPY;
                 cfo.height = vcs.v_active-1; /* tty1 == index 0 */
                 ioctl(vcfd, KDFONTOP, &cfo);
-
-                /* copy map of 8bit chars */
-                if (ioctl(fd, GIO_SCRNMAP, map8) >= 0)
-                    ioctl(vcfd, PIO_SCRNMAP, map8);
-
-                /* copy map of 8bit chars -> 16bit Unicode values */
-                if (ioctl(fd, GIO_UNISCRNMAP, map16) >= 0)
-                    ioctl(vcfd, PIO_UNISCRNMAP, map16);
 
                 /* copy unicode translation table */
                 /* unimapd is a ushort count and a pointer to an
