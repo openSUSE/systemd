openSUSE/systemd

commit 1e5bf16bd88c859f9128f05adf3c840f3c21ba2a

systemd-mount: set device timeout from the kernel commandline
On some systems device probing can take longer than the predefined
device timeout. This patch adds a kernel commandline option
'mount.timeout' and 'rd.timeout' for allowing to modify the
default device timeout.

Signed-off-by: Hannes Reinecke <hare@suse.de>

src/core/mount.c
@@ -440,7 +440,7 @@ static int mount_fix_timeouts(Mount *m) {
         Unit *other;
         Iterator i;
         usec_t u;
-        char *t;
+        char *t = NULL;
         int r;
 
         assert(m);
@@ -458,12 +458,40 @@ static int mount_fix_timeouts(Mount *m) {
                 timeout += 31;
         else if ((timeout = mount_test_option(p->options, "x-systemd.device-timeout")))
                 timeout += 25;
-        else
-                return 0;
 
-        t = strndup(timeout, strcspn(timeout, ",;" WHITESPACE));
+        if (timeout) {
+                t = strndup(timeout, strcspn(timeout, ",;" WHITESPACE));
+                if (!t)
+                        return -ENOMEM;
+        } else {
+                _cleanup_free_ char *line = NULL;
+                char *w, *state;
+                size_t l;
+
+                r = proc_cmdline(&line);
+                if (r > 0) {
+                        /*
+                         * Allow to override the device timeout from the
+                         * kernel commandline, allowing later entries
+                         * to override earlier ones.
+                         */
+                        FOREACH_WORD_QUOTED(w, l, line, state) {
+                                if (startswith(w, "mount.timeout=")) {
+                                        if (t)
+                                                free(t);
+                                        t = strdup(w + 14);
+                                } else if (startswith(w, "rd.timeout=")) {
+                                        if (in_initrd()) {
+                                                if (t)
+                                                        free(t);
+                                                t = strdup(w + 11);
+                                        }
+                                }
+                        }
+                }
+        }
         if (!t)
-                return -ENOMEM;
+                return 0;
 
         r = parse_sec(t, &u);
         free(t);

man/kernel-command-line.xml
@@ -292,6 +292,17 @@
                         </varlistentry>
 
                         <varlistentry>
+                                <term><varname>mount.timeout=</varname></term>
+                                <term><varname>rd.timeout=</varname></term>
+
+                                <listitem>
+                                        <para>Configures the device timeout
+                                        logic at boot. For details, see
+                                        <citerefentry><refentrytitle>systemd-fstab-generator</refentrytitle><manvolnum>8</manvolnum></citerefentry>.</para>
+                                </listitem>
+                        </varlistentry>
+
+                        <varlistentry>
                                 <term><varname>modules-load=</varname></term>
                                 <term><varname>rd.modules-load=</varname></term>
 

man/systemd-fstab-generator.xml
@@ -106,6 +106,22 @@
                                 honored by both the main system and
                                 the initrd.  </para></listitem>
                         </varlistentry>
+                        <varlistentry>
+                                <term><varname>mount.timeout=</varname></term>
+                                <term><varname>rd.timeout=</varname></term>
+
+                                <listitem><para>Specifies the device
+                                timeout in seconds. It will only be
+                                applied if no device-specific timeout
+                                settings in
+                                <filename>/etc/fstab</filename> are configured.
+                                <varname>rd.timeout</varname> is
+                                honoured only by initial RAMD disk
+                                (initrd) while
+                                <varname>mount.timeout=</varname> is
+                                honored by both the main system and
+                                the initrd.  </para></listitem>
+                        </varlistentry>
 
                 </variablelist>
         </refsect1>

commit 5661999628663f32cbbb32c7653af543f50430f4

core: fix parsing of mount.timeout in mount-fix-timeouts()
The strings returned by FOREACH_WORD_QUOTED are not null-terminated.

Fixes: 1e5bf16 "systemd-mount: set device timeout from the kernel commandline"

[fbui: this bug was discovered while addressing bsc#1023220]

src/core/mount.c
@@ -495,12 +495,12 @@ static int mount_fix_timeouts(Mount *m) {
                                 if (startswith(w, "mount.timeout=")) {
                                         if (t)
                                                 free(t);
-                                        t = strdup(w + 14);
+                                        t = strndup(w + 14, l - 14);
                                 } else if (startswith(w, "rd.timeout=")) {
                                         if (in_initrd()) {
                                                 if (t)
                                                         free(t);
-                                                t = strdup(w + 11);
+                                                t = strndup(w + 11, l - 11);
                                         }
                                 }
                         }