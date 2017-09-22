101975f2747e8393b5e321e618d8283b457bc2de
Lennart Poettering <lennart@poettering.net>
2017-09-22 17:55:53 +0200

    fileio: add new helper call read_line() as bounded getline() replacement
    read_line() is much like getline(), and returns a line read from a
    FILE*, of arbitrary sizes. In contrast to gets() it will grow the buffer
    dynamically, and in contrast to getline() it will place a user-specified
    boundary on the line.
    
    (cherry picked from commit 4f9a66a32dda1d9a28f9bb3fa31c2148524bc46a)
    
    [fbui: adjust context]

--- src/basic/fileio.h
+++ src/basic/fileio.h
@@ -82,3 +82,5 @@ int tempfn_random_child(const char *p, const char *extra, char **ret);
 
 int write_timestamp_file_atomic(const char *fn, usec_t n);
 int read_timestamp_file(const char *fn, usec_t *ret);
+
+int read_line(FILE *f, size_t limit, char **ret);
