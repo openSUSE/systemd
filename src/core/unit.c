691518351e3b847d5a8788cc1753163afc10206d
Franck Bui <fbui@suse.com>
2018-02-07 14:08:02 +0100

    core: use id unit when retrieving unit file state (#8038)
    Previous code was using the basename(id->fragment_path) which returned
    incorrect result if the unit was an instance.
    
    For example, assuming that no instances of "template" have been created so far:
    
     $ systemctl enable template@1
     Created symlink from /etc/systemd/system/multi-user.target.wants/template@1.service to /usr/lib/systemd/system/template@.service.
    
     $ systemctl is-enabled template@3.service
     disabled
    
     $ systemctl status template@3.service
     ● template@3.service - openQA Worker #3
        Loaded: loaded (/usr/lib/systemd/system/template@.service; enabled; vendor preset: disabled)
        [...]
    
    Here the unit file states reported by "status" and "is-enabled" were different.
    
    (cherry picked from commit 9ea3a0e702e741e5a6015093dd02a2374722a62d)
    
    [fbui: fixes bsc#1075801]

--- src/core/unit.c
+++ src/core/unit.c
@@ -3203,7 +3203,7 @@ UnitFileState unit_get_unit_file_state(Unit *u) {
                 r = unit_file_get_state(
                                 u->manager->running_as == MANAGER_SYSTEM ? UNIT_FILE_SYSTEM : UNIT_FILE_USER,
                                 NULL,
-                                basename(u->fragment_path),
+                                u->id,
                                 &u->unit_file_state);
                 if (r < 0)
                         u->unit_file_state = UNIT_FILE_BAD;
