fc7a9bd063898e5626c6735befc142de033a6a26
Franck Bui <fbui@suse.com>
2018-02-23 17:12:50 +0100

    rule-syntax-check: values can contain escaped double quotes
    This is true since commit 7e760b79ad143b26a5c937afa7666a7c40508f85.
    
    Note that the changes in the regex expressions relies on the fact that the
    script assumes that the comma separator is mandatory.
    
    Add a comment in the script to clarify this.
    
    (cherry picked from commit 75a56cb63259505e568d0524461d446040ceba6c)

--- test/rule-syntax-check.py
+++ test/rule-syntax-check.py
@@ -33,10 +33,10 @@ else:
         sys.exit(2)
     rules_files = glob(os.path.join(rules_dir, '*.rules'))
 
-no_args_tests = re.compile(r'(ACTION|DEVPATH|KERNELS?|NAME|SYMLINK|SUBSYSTEMS?|DRIVERS?|TAG|PROGRAM|RESULT|TEST)\s*(?:=|!)=\s*"([^"]*)"$')
-args_tests = re.compile(r'(ATTRS?|ENV|TEST){([a-zA-Z0-9/_.*%-]+)}\s*(?:=|!)=\s*"([^"]*)"$')
-no_args_assign = re.compile(r'(NAME|SYMLINK|OWNER|GROUP|MODE|TAG|RUN|LABEL|GOTO|OPTIONS|IMPORT)\s*(?:\+=|:=|=)\s*"([^"]*)"$')
-args_assign = re.compile(r'(ATTR|ENV|IMPORT|RUN){([a-zA-Z0-9/_.*%-]+)}\s*(=|\+=)\s*"([^"]*)"$')
+no_args_tests = re.compile(r'(ACTION|DEVPATH|KERNELS?|NAME|SYMLINK|SUBSYSTEMS?|DRIVERS?|TAG|PROGRAM|RESULT|TEST)\s*(?:=|!)=\s*"(.*)"$')
+args_tests = re.compile(r'(ATTRS?|ENV|TEST){([a-zA-Z0-9/_.*%-]+)}\s*(?:=|!)=\s*"(.*)"$')
+no_args_assign = re.compile(r'(NAME|SYMLINK|OWNER|GROUP|MODE|TAG|RUN|LABEL|GOTO|OPTIONS|IMPORT)\s*(?:\+=|:=|=)\s*"(.*)"$')
+args_assign = re.compile(r'(ATTR|ENV|IMPORT|RUN){([a-zA-Z0-9/_.*%-]+)}\s*(=|\+=)\s*"(.*)"$')
 
 result = 0
 buffer = ''
@@ -58,6 +58,8 @@ for path in rules_files:
         if not line or line.startswith('#'):
             continue
 
+        # Separator ',' is normally optional but we make it mandatory here as
+        # it generally improves the readability of the rules.
         for clause in line.split(','):
             clause = clause.strip()
             if not (no_args_tests.match(clause) or args_tests.match(clause) or
