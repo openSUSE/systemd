a1ba1e298b73a404323219c7bc28adb453871223
Franck Bui <fbui@suse.com>
2018-04-11 16:58:49 +0200

    fs-util: introduce fchmod_opath()
    fchmod(2) still doesn't take file descriptors opened with O_PATH.
    
    (cherry picked from commit 4dfaa528d451aa7926be4f1b4cf8d0ffe338421d)
    
    [fbui: adjust context]

--- src/basic/fs-util.h
+++ src/basic/fs-util.h
@@ -45,6 +45,7 @@ int readlink_and_make_absolute_root(const char *root, const char *path, char **r
 int chmod_and_chown(const char *path, mode_t mode, uid_t uid, gid_t gid);
 
 int fchmod_umask(int fd, mode_t mode);
+int fchmod_opath(int fd, mode_t m);
 
 int fd_warn_permissions(const char *path, int fd);
 
