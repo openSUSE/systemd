a516490b24a76c3cd6a29f23117acee339b36a92
Lennart Poettering <lennart@poettering.net>
2018-05-03 15:47:42 +0200

    test: don't send image building output to /dev/null (#8886)
    Yes, the output is sometimes annyoing, but /dev/null is not the right
    place...
    
    I figure this redirection was left in from some debugging session, let's
    fix it, and make the setup_basic_environment invocation like in all
    other test scripts.
    
    (cherry picked from commit 0ea21d9e88e40452c741940a54e5db244d3a04e4)

--- test/TEST-15-DROPIN/test.sh
+++ test/TEST-15-DROPIN/test.sh
@@ -21,7 +21,7 @@ test_run() {
 
 test_setup() {
         # create the basic filesystem layout
-        setup_basic_environment >/dev/null
+        setup_basic_environment
 
         # mask some services that we do not want to run in these tests
         ln -s /dev/null $initdir/etc/systemd/system/systemd-hwdb-update.service
