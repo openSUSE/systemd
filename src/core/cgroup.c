6c0efa2f01319cd6b4013f666e25a0dcc798071c
Lennart Poettering <lennart@poettering.net>
2018-11-20 19:48:43 +0100

    cgroup: suffix settings with "=" in log messages where appropriate
    (cherry picked from commit 73fe5314bf0cc220da3487d9be322e46e674e692)

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -905,7 +905,7 @@ static void cgroup_context_apply(
                                 max = c->memory_limit;
 
                                 if (max != CGROUP_LIMIT_MAX)
-                                        log_cgroup_compat(u, "Applying MemoryLimit %" PRIu64 " as MemoryMax", max);
+                                        log_cgroup_compat(u, "Applying MemoryLimit=%" PRIu64 " as MemoryMax=", max);
                         }
 
                         cgroup_apply_unified_memory_limit(u, "memory.low", c->memory_low);
@@ -918,7 +918,7 @@ static void cgroup_context_apply(
 
                         if (cgroup_context_has_unified_memory_config(c)) {
                                 val = c->memory_max;
-                                log_cgroup_compat(u, "Applying MemoryMax %" PRIi64 " as MemoryLimit", val);
+                                log_cgroup_compat(u, "Applying MemoryMax=%" PRIi64 " as MemoryLimit=", val);
                         } else
                                 val = c->memory_limit;
 
