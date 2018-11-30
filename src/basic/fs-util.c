d9ae1b30da0219f004e266e0baffa8e059342ef1
Franck Bui <fbui@suse.com>
2018-11-30 15:13:44 +0100

    fs-util: make chase_symlink() returns -ENOLINK when unsafe transitions are met
    We previously returned -EPERM but it can be returned for various other reasons
    too.
    
    Let's use -ENOLINK instead as this value shouldn't be used currently. This
    allows users of CHASE_SAFE to detect without any ambiguities when unsafe
    transitions are encountered by chase_symlinks().
    
    All current users of CHASE_SAFE that explicitly reacted on -EPERM have been
    converted to react on -ENOLINK.
    
    (cherry picked from commit 36c97decbe46a62b6976b8ba7c59e8552f14f935)
    
    [fbui: adjust context]

--- src/basic/fs-util.c
+++ src/basic/fs-util.c
@@ -631,14 +631,14 @@ static int log_unsafe_transition(int a, int b, const char *path, unsigned flags)
         _cleanup_free_ char *n1 = NULL, *n2 = NULL;
 
         if (!FLAGS_SET(flags, CHASE_WARN))
-                return -EPERM;
+                return -ENOLINK;
 
         (void) fd_get_path(a, &n1);
         (void) fd_get_path(b, &n2);
 
         log_warning("Detected unsafe path transition %s %s %s during canonicalization of %s.",
                     n1, special_glyph(ARROW), n2, path);
-        return -EPERM;
+        return -ENOLINK;
 }
 
 int chase_symlinks(const char *path, const char *original_root, unsigned flags, char **ret) {
@@ -700,6 +700,10 @@ int chase_symlinks(const char *path, const char *original_root, unsigned flags, 
          *    a caller wants to trace the a path through the file system verbosely. Returns < 0 on error, > 0 if the
          *    path is fully normalized, and == 0 for each normalization step. This may be combined with
          *    CHASE_NONEXISTENT, in which case 1 is returned when a component is not found.
+         *
+         * 4. With CHASE_SAFE: in this case the path must not contain unsafe transitions, i.e. transitions from
+         *    unprivileged to privileged files or directories. In such cases the return value is -ENOLINK. If
+         *    CHASE_WARN is also set a warning describing the unsafe transition is emitted.
          *
          * */
 
