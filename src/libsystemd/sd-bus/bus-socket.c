3dba60375bb0cef7268544f107ced7cb278bf4fe
Lennart Poettering <lennart@poettering.net>
2019-02-13 16:51:22 +0100

    sd-bus: if we receive an invalid dbus message, ignore and proceeed
    dbus-daemon might have a slightly different idea of what a valid msg is
    than us (for example regarding valid msg and field sizes). Let's hence
    try to proceed if we can and thus drop messages rather than fail the
    connection if we fail to validate a message.
    
    Hopefully the differences in what is considered valid are not visible
    for real-life usecases, but are specific to exploit attempts only.
    
    (cherry picked from commit 6d586a13717ae057aa1b4127400c3de61cd5b9e7)
    
    [fbui: adjust context]

--- src/libsystemd/sd-bus/bus-socket.c
+++ src/libsystemd/sd-bus/bus-socket.c
@@ -896,7 +896,7 @@ static int bus_socket_read_message_need(sd_bus *bus, size_t *need) {
 }
 
 static int bus_socket_make_message(sd_bus *bus, size_t size) {
-        sd_bus_message *t;
+        sd_bus_message *t = NULL;
         void *b;
         int r;
 
@@ -922,7 +922,9 @@ static int bus_socket_make_message(sd_bus *bus, size_t size) {
                                     !bus->bus_client && bus->ucred_valid ? &bus->ucred : NULL,
                                     !bus->bus_client && bus->label[0] ? bus->label : NULL,
                                     &t);
-        if (r < 0) {
+        if (r == -EBADMSG)
+                log_debug("Received invalid message, dropping.");
+        else if (r < 0) {
                 free(b);
                 return r;
         }
@@ -933,7 +935,8 @@ static int bus_socket_make_message(sd_bus *bus, size_t size) {
         bus->fds = NULL;
         bus->n_fds = 0;
 
-        bus->rqueue[bus->rqueue_size++] = t;
+        if (t)
+                bus->rqueue[bus->rqueue_size++] = t;
 
         return 1;
 }
