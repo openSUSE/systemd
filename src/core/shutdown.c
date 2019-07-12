1130a2a7126758b25fb2ce55e314bb7cacd9eb3f
Lennart Poettering <lennart@poettering.net>
2019-07-12 10:41:28 +0200

    shutdown: bump kmsg log level to LOG_WARNING only
    Fixes: #12337
    (cherry picked from commit 9a75c652488bfe425958e367a15bc1dd8d1bd82c)
    
    [fbui: adjust context]

--- src/core/shutdown.c
+++ src/core/shutdown.c
@@ -352,15 +352,17 @@ int main(int argc, char *argv[]) {
 
         use_watchdog = !!getenv("WATCHDOG_USEC");
 
-        /* If the logging messages are going to KMSG, and if we are not running from a container,
-         * then try to update the sysctl kernel.printk current value in order to see "info" messages;
-         * This current log level is not updated if already big enough.
+        /* If the logging messages are going to KMSG, and if we are not running from a container, then try to
+         * update the sysctl kernel.printk current value in order to see "info" messages; This current log
+         * level is not updated if already big enough.
          */
-        if (!in_container && IN_SET(log_get_target(), LOG_TARGET_AUTO,
-                                                      LOG_TARGET_JOURNAL_OR_KMSG,
-                                                      LOG_TARGET_SYSLOG_OR_KMSG,
-                                                      LOG_TARGET_KMSG))
-                bump_sysctl_printk_log_level(LOG_INFO);
+        if (!in_container &&
+            IN_SET(log_get_target(),
+                   LOG_TARGET_AUTO,
+                   LOG_TARGET_JOURNAL_OR_KMSG,
+                   LOG_TARGET_SYSLOG_OR_KMSG,
+                   LOG_TARGET_KMSG))
+                bump_sysctl_printk_log_level(LOG_WARNING);
 
         /* Lock us into memory */
         mlockall(MCL_CURRENT|MCL_FUTURE);
