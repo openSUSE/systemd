49e51f69b57c50a564b37a7514fc4842fac8c6ff
Franck Bui <fbui@suse.com>
2019-09-17 11:16:52 +0200

    fs-util: introduce inotify_add_watch_and_warn() helper
    The default message for ENOSPC is very misleading: it says that the disk is
    filled, but in fact the inotify watch limit is the problem.
    
    So let's introduce and use a wrapper that simply calls inotify_add_watch(2) and
    which fixes the error message up in case ENOSPC is returned.
    
    (cherry picked from commit 27c3112dcbd1b5f171c36c32550d9c6331375b0b)
    
    [fbui: adjust context]
    [fbui: follow-up for bsc#1155574]

--- src/basic/fs-util.h
+++ src/basic/fs-util.h
@@ -79,6 +79,7 @@ union inotify_event_buffer {
 };
 
 int inotify_add_watch_fd(int fd, int what, uint32_t mask);
+int inotify_add_watch_and_warn(int fd, const char *pathname, uint32_t mask);
 
 enum {
         CHASE_PREFIX_ROOT = 1U << 0,   /* If set, the specified path will be prefixed by the specified root before beginning the iteration */
