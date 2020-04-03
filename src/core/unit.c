cc5170fcefcd0310988f60cad93e8f439aae2dd8
Franck Bui <fbui@suse.com>
2020-04-03 10:00:25 +0200

    pid1: by default make user units inherit their umask from the user manager
    This patch changes the way user managers set the default umask for the units it
    manages.
    
    Indeed one can expect that if user manager's umask is redefined through PAM
    (via /etc/login.defs or pam_umask), all its children including the units it
    spawns have their umask set to the new value.
    
    Hence make user units inherit their umask value from their parent instead of
    the hard coded value 0022 but allow them to override this value via their unit
    file.
    
    Note that reexecuting managers with 'systemctl daemon-reexec' after changing
    UMask= has no effect. To take effect managers need to be restarted with
    'systemct restart' instead. This behavior was already present before this
    patch.
    
    Fixes #6077.
    
    (cherry picked from commit 5e37d1930b41b24c077ce37c6db0e36c745106c7)
    
    [fbui: fixes bsc#1162698]

--- src/core/unit.c
+++ src/core/unit.c
@@ -173,8 +173,16 @@ static void unit_init(Unit *u) {
         }
 
         ec = unit_get_exec_context(u);
-        if (ec)
+        if (ec) {
                 exec_context_init(ec);
+
+                if (MANAGER_IS_USER(u->manager)) {
+                        /* User manager might have its umask redefined by PAM or UMask=. In this
+                         * case let the units it manages inherit this value by default. They can
+                         * still tune this value through their own unit file */
+                        (void) get_process_umask(getpid(), &ec->umask);
+                }
+        }
 
         kc = unit_get_kill_context(u);
         if (kc)
