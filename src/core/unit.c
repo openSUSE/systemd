ef8afc45455fe183a69f66c99c1e434839611ffd
Michal Koutný <mkoutny@suse.com>
2021-08-04 18:59:35 +0200

    core: Make sure cgroup_oom_queue is flushed on manager exit
    The unit queues are not serialized/deserialized (they are recreated
    after reexec/reload instead). The destroyed units are not removed from
    the cgroup_oom_queue. That means the queue may contain possibly invalid
    pointers to released units.
    
    Fix this by removing the units from cgroup_oom_queue as we do for
    others. When at it, sync assert checks with currently existing queues
    and put them in order in the manager cleanup code.
    
    (cherry picked from commit 13e721036bf4ba15eb255d8f0a14800f969ac0d7)

--- src/core/unit.c
+++ src/core/unit.c
@@ -697,6 +697,9 @@ void unit_free(Unit *u) {
         if (u->in_dbus_queue)
                 LIST_REMOVE(dbus_queue, u->manager->dbus_unit_queue, u);
 
+        if (u->in_cleanup_queue)
+                LIST_REMOVE(cleanup_queue, u->manager->cleanup_queue, u);
+
         if (u->in_gc_queue)
                 LIST_REMOVE(gc_queue, u->manager->gc_unit_queue, u);
 
@@ -706,8 +709,8 @@ void unit_free(Unit *u) {
         if (u->in_cgroup_empty_queue)
                 LIST_REMOVE(cgroup_empty_queue, u->manager->cgroup_empty_queue, u);
 
-        if (u->in_cleanup_queue)
-                LIST_REMOVE(cleanup_queue, u->manager->cleanup_queue, u);
+        if (u->in_cgroup_oom_queue)
+                LIST_REMOVE(cgroup_oom_queue, u->manager->cgroup_oom_queue, u);
 
         if (u->in_target_deps_queue)
                 LIST_REMOVE(target_deps_queue, u->manager->target_deps_queue, u);
