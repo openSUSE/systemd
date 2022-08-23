533e98fc6b829e599bcb68d76181dabb2c244e16
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-08-23 16:24:02 +0200

    sd-device: add helper to read a unsigned int attribute
    There are dozens of places where this could be used, but I don't
    want to do the conversion now because it's quite a bit of work.
    I think we could export this function later on, because reading
    numerical attributes is so common. But for now, I'm just adding the
    helper to use it one place.
    
    (cherry picked from commit 48a511cf92cbf202e9ef6064a9b5ebd1f497e1a8)
    
    [fbui: adjust context]

--- src/libsystemd/sd-device/device-private.h
+++ src/libsystemd/sd-device/device-private.h
@@ -17,6 +17,7 @@ static inline int device_new_from_watch_handle(sd_device **ret, int wd) {
         return device_new_from_watch_handle_at(ret, -1, wd);
 }
 
+int device_get_sysattr_unsigned(sd_device *device, const char *sysattr, unsigned *ret_value);
 int device_get_device_id(sd_device *device, const char **ret);
 
 int device_get_devlink_priority(sd_device *device, int *priority);
