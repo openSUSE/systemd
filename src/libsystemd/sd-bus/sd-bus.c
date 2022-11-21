7d874abc1c35c19f00da996180bb6f967351bdad
Lennart Poettering <lennart@poettering.net>
2022-11-21 17:42:04 +0100

    sd-bus: handle -EINTR return from bus_poll()
    In sd_bus_wait(), let's convert EINTR to a return code of 0, thus asking
    the caller do loop again and enter sd_bus_process() again (which will
    not find any queued events). This way we'll not return an error on
    something that isn't really an error. This should typically make sure
    things are properly handled by the caller, magically, without eating up
    the event entirely, and still giving the caller time to run some code if
    they want.
    
    (cherry picked from commit 3022916b4d2483452c3ddbbac9ee7c4372b1cb46)
    
    [fbui: fixes bsc#1215241]
    [fbui: adjust context]

--- src/libsystemd/sd-bus/sd-bus.c
+++ src/libsystemd/sd-bus/sd-bus.c
@@ -2278,8 +2278,11 @@ _public_ int sd_bus_call(
                         left = (uint64_t) -1;
 
                 r = bus_poll(bus, true, left);
-                if (r < 0)
+                if (r < 0) {
+                        if (ERRNO_IS_TRANSIENT(r))
+                                continue;
                         goto fail;
+                }
                 if (r == 0) {
                         r = -ETIMEDOUT;
                         goto fail;
@@ -3144,6 +3147,7 @@ static int bus_poll(sd_bus *bus, bool need_more, uint64_t timeout_usec) {
 }
 
 _public_ int sd_bus_wait(sd_bus *bus, uint64_t timeout_usec) {
+        int r;
 
         assert_return(bus, -EINVAL);
         assert_return(bus = bus_resolve(bus), -ENOPKG);
@@ -3158,7 +3162,11 @@ _public_ int sd_bus_wait(sd_bus *bus, uint64_t timeout_usec) {
         if (bus->rqueue_size > 0)
                 return 0;
 
-        return bus_poll(bus, false, timeout_usec);
+        r = bus_poll(bus, false, timeout_usec);
+        if (r < 0 && ERRNO_IS_TRANSIENT(r))
+                return 1; /* treat EINTR as success, but let's exit, so that the caller will call back into us soon. */
+
+        return r;
 }
 
 _public_ int sd_bus_flush(sd_bus *bus) {
@@ -3200,8 +3208,12 @@ _public_ int sd_bus_flush(sd_bus *bus) {
                         return 0;
 
                 r = bus_poll(bus, false, (uint64_t) -1);
-                if (r < 0)
+                if (r < 0) {
+                        if (ERRNO_IS_TRANSIENT(r))
+                                continue;
+
                         return r;
+                }
         }
 }
 
