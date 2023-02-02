d205cdc59f1e668a2489711756891d71da069b3c
Dan Streetman <ddstreet@ieee.org>
2023-02-02 15:58:10 -0500

    basic/macro: add macro to iterate variadic args
    (cherry picked from commit e179f2d89c9f0c951636d74de00136b4075cd1ac)
    
    [fbui: adjust context]

--- src/basic/macro.h
+++ src/basic/macro.h
@@ -635,4 +635,13 @@ static inline size_t size_add(size_t x, size_t y) {
         return y >= SIZE_MAX - x ? SIZE_MAX : x + y;
 }
 
+/* Iterate through each variadic arg. All must be the same type as 'entry' or must be implicitly
+ * convertable. The iteration variable 'entry' must already be defined. */
+#define VA_ARGS_FOREACH(entry, ...)                                     \
+        _VA_ARGS_FOREACH(entry, UNIQ_T(_entries_, UNIQ), UNIQ_T(_current_, UNIQ), ##__VA_ARGS__)
+#define _VA_ARGS_FOREACH(entry, _entries_, _current_, ...)         \
+        for (typeof(entry) _entries_[] = { __VA_ARGS__ }, *_current_ = _entries_; \
+             ((long)(_current_ - _entries_) < (long)ELEMENTSOF(_entries_)) && ({ entry = *_current_; true; }); \
+             _current_++)
+
 #include "log.h"
