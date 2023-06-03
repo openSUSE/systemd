61f67f0bd1311290ba43e0fb43d214604b10fe69
Lennart Poettering <lennart@poettering.net>
2023-06-03 09:17:45 +0200

    umount: /usr/ should never be unmounted regardless of HAVE_SPLIT_USR or not
    Whether we put some binaries in /bin or in /usr/bin should not have any
    effect on unmounting during shutdown. Even if people split /usr/ off we
    should not try to unmount it, no matter what as it is simply where
    binaries are placed.
    
    (cherry picked from commit 798e811733f4a85bf51672ce8bcdb649a1173711)
    
    [fbui: fixes bsc#1211576]

--- src/shutdown/umount.c
+++ src/shutdown/umount.c
@@ -413,11 +413,10 @@ static int delete_dm(dev_t devnum) {
 }
 
 static bool nonunmountable_path(const char *path) {
-        return path_equal(path, "/")
-#if ! HAVE_SPLIT_USR
-                || path_equal(path, "/usr")
-#endif
-                || path_startswith(path, "/run/initramfs");
+        assert(path);
+
+        return PATH_IN_SET(path, "/", "/usr") ||
+                path_startswith(path, "/run/initramfs");
 }
 
 static int remount_with_timeout(MountPoint *m, int umount_log_level) {
