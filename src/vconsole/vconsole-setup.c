6f53f71d2da2c75275c12f1cfbeb51d8e9229560
Lennart Poettering <lennart@poettering.net>
2023-08-08 14:20:17 +0200

    vconsole-setup: simplify error handling
    no need to negate an error which we don't need negated.
    
    also, add debug log about errors we ignore.
    
    (cherry picked from commit 6433f1f6a29fd5a063bf2e043421c01d018ce928)

--- src/vconsole/vconsole-setup.c
+++ src/vconsole/vconsole-setup.c
@@ -520,22 +520,22 @@ static int find_source_vc(char **ret_path, unsigned *ret_idx) {
 
                 r = verify_vc_allocation(i);
                 if (r < 0) {
-                        if (!err)
-                                err = -r;
+                        log_debug_errno(r, "VC %u existance check failed, skipping: %m", i);
+                        RET_GATHER(err, r);
                         continue;
                 }
 
                 sprintf(path, "/dev/tty%u", i);
                 fd = open_terminal(path, O_RDWR|O_CLOEXEC|O_NOCTTY);
                 if (fd < 0) {
-                        if (!err)
-                                err = -fd;
+                        log_debug_errno(fd, "Failed to open terminal %s, ignoring: %m", path);
+                        RET_GATHER(err, r);
                         continue;
                 }
                 r = verify_vc_kbmode(fd);
                 if (r < 0) {
-                        if (!err)
-                                err = -r;
+                        log_debug_errno(r, "Failed to check VC %s keyboard mode: %m", path);
+                        RET_GATHER(err, r);
                         continue;
                 }
 
