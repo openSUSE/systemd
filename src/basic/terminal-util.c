48c1393bfb026e114aed56f1c2b8564662039c9b
Mike Yuan <me@yhndnzj.com>
2026-02-10 22:21:25 +0100

    terminal-util: also protect from empty /sys/class/tty/tty0/active

--- src/basic/terminal-util.c
+++ src/basic/terminal-util.c
@@ -1151,6 +1151,8 @@ int resolve_dev_console(char **ret) {
                 r = read_one_line_file("/sys/class/tty/tty0/active", &active);
                 if (r < 0)
                         return r;
+                if (r == 0)
+                        return -ENXIO;
 
                 tty = active;
         }
@@ -1194,6 +1196,10 @@ int get_kernel_consoles(char ***ret) {
                         r = read_one_line_file("/sys/class/tty/tty0/active", &tty);
                         if (r < 0)
                                 return r;
+                        if (r == 0) {
+                                log_debug("No VT active, skipping /dev/tty0.");
+                                continue;
+                        }
                 }
 
                 path = path_join("/dev", tty);
