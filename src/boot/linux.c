353701b07e6750282461d6d61a3d8bffffe3ebbc
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-02-17 18:21:45 +0100

    boot: drop now-unused image_base param

--- src/boot/linux.c
+++ src/boot/linux.c
@@ -167,7 +167,7 @@ EFI_STATUS linux_exec(
         assert(iovec_is_set(kernel));
         assert(iovec_is_valid(initrd));
 
-        err = pe_kernel_info(kernel->iov_base, &entry_point, &compat_entry_point, /* ret_image_base= */ NULL, &kernel_size_in_memory);
+        err = pe_kernel_info(kernel->iov_base, &entry_point, &compat_entry_point, &kernel_size_in_memory);
 #if defined(__i386__) || defined(__x86_64__)
         if (err == EFI_UNSUPPORTED)
                 /* Kernel is too old to support LINUX_INITRD_MEDIA_GUID, try the deprecated EFI handover
