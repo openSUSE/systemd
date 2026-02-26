aef6e11921f8c46a2b7ee8cfab024c9c641d74d8
Mike Yuan <me@yhndnzj.com>
2026-02-26 11:06:00 +0100

    core/cgroup: avoid one unnecessary strjoina()
    (cherry picked from commit 42aee39107fbdd7db1ccd402a2151822b2805e9f)
    
    [fbui: adjust context]

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -2404,12 +2404,13 @@ static int unit_update_cgroup(
         return 0;
 }
 
-static int unit_attach_pid_to_cgroup_via_bus(Unit *u, pid_t pid, const char *suffix_path) {
+static int unit_attach_pid_to_cgroup_via_bus(Unit *u, const char *cgroup_path, pid_t pid) {
         _cleanup_(sd_bus_error_free) sd_bus_error error = SD_BUS_ERROR_NULL;
-        char *pp;
         int r;
 
         assert(u);
+        assert(cgroup_path);
+        assert(pid_is_valid(pid));
 
         if (MANAGER_IS_SYSTEM(u->manager))
                 return -EINVAL;
@@ -2417,16 +2418,14 @@ static int unit_attach_pid_to_cgroup_via_bus(Unit *u, pid_t pid, const char *suf
         if (!u->manager->system_bus)
                 return -EIO;
 
-        if (!u->cgroup_path)
-                return -EINVAL;
-
         /* Determine this unit's cgroup path relative to our cgroup root */
-        pp = path_startswith(u->cgroup_path, u->manager->cgroup_root);
-        if (!pp)
+        const char *pp = path_startswith_full(cgroup_path,
+                                              u->manager->cgroup_root,
+                                              /* accept_dot_dot= */ false);
+        if (!pp || pp <= cgroup_path)
                 return -EINVAL;
-
-        pp = strjoina("/", pp, suffix_path);
-        path_simplify(pp);
+        if (*--pp != '/')
+                return -EINVAL;
 
         r = bus_call_method(u->manager->system_bus,
                             bus_systemd_mgr,
@@ -2466,8 +2465,10 @@ int unit_attach_pids_to_cgroup(Unit *u, Set *pids, const char *suffix_path) {
                 return r;
 
         if (isempty(suffix_path))
-                p = u->cgroup_path;
+                p = empty_to_root(u->cgroup_path);
         else {
+                assert(path_is_absolute(suffix_path));
+
                 joined = path_join(u->cgroup_path, suffix_path);
                 if (!joined)
                         return -ENOMEM;
@@ -2488,7 +2489,7 @@ int unit_attach_pids_to_cgroup(Unit *u, Set *pids, const char *suffix_path) {
 
                         log_unit_full_errno(u, again ? LOG_DEBUG : LOG_INFO,  r,
                                             "Couldn't move process "PID_FMT" to%s requested cgroup '%s': %m",
-                                            pid, again ? " directly" : "", empty_to_root(p));
+                                            pid, again ? " directly" : "", p);
 
                         if (again) {
                                 int z;
@@ -2498,9 +2499,9 @@ int unit_attach_pids_to_cgroup(Unit *u, Set *pids, const char *suffix_path) {
                                  * Since it's more privileged it might be able to move the process across the
                                  * leaves of a subtree whose top node is not owned by us. */
 
-                                z = unit_attach_pid_to_cgroup_via_bus(u, pid, suffix_path);
+                                z = unit_attach_pid_to_cgroup_via_bus(u, p, pid);
                                 if (z < 0)
-                                        log_unit_info_errno(u, z, "Couldn't move process "PID_FMT" to requested cgroup '%s' (directly or via the system bus): %m", pid, empty_to_root(p));
+                                        log_unit_info_errno(u, z, "Couldn't move process "PID_FMT" to requested cgroup '%s' (directly or via the system bus): %m", pid, p);
                                 else {
                                         if (ret >= 0)
                                                 ret++; /* Count successful additions */
