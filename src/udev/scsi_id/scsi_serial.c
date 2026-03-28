8f9f801372c86d308fb3c2140c2a6197bdd010b5
Luca Boccassi <luca.boccassi@gmail.com>
2026-03-28 19:35:36 +0000

    scsi_id: use strscpy instead of strncpy for wwn fields
    strncpy does not null-terminate the destination buffer if the source
    string is longer than the count parameter. Since wwn and
    wwn_vendor_extension are char[17] and we copy up to 16 bytes, there's
    a risk of missing null termination. Use strscpy which always
    null-terminates.
    
    CID#1469706
    
    Follow-up for 4e9fdfccbdd16f0cfdb5c8fa8484a8ba0f2e69d3
    
    (cherry picked from commit 86fd0337c652b04755008cdca23e2d9c727fa9a9)
    
    [fbui: adjust context]

--- src/udev/scsi_id/scsi_serial.c
+++ src/udev/scsi_id/scsi_serial.c
@@ -26,6 +26,7 @@
 #include "scsi.h"
 #include "scsi_id.h"
 #include "string-util.h"
+#include "strxcpyx.h"
 
 /*
  * A priority based list of id, naa, and binary/ascii for the identifier
@@ -523,9 +524,9 @@ static int check_fill_0x83_id(struct scsi_id_device *dev_scsi,
         strcpy(serial_short, serial + s);
 
         if (id_search->id_type == SCSI_ID_NAA && wwn != NULL) {
-                strncpy(wwn, serial + s, 16);
+                strscpy(wwn, 17, serial + s);
                 if (wwn_vendor_extension)
-                        strncpy(wwn_vendor_extension, serial + s + 16, 16);
+                        strscpy(wwn_vendor_extension, 17, serial + s + 16);
         }
 
         return 0;
